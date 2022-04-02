f5d7b04736934d70a40f7476aaad789fd137dc04
Thiago Hagge <thiagohagge09@gmail.com>
2022-04-01 21:41:55 -0300

    remove swr

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
-import useSWR from "swr";
 
 import api from '../src/api';
 import theme from '../src/themes';
@@ -13,40 +12,9 @@ import Layout from '../src/patterns/Layout';
 import Videos from '../src/patterns/Videos'
 import News from '../src/patterns/News';
 
-export default function Home() {
-	const [carouselItems, setCarouselItems] = useState([]);
-	const [news, setNews] = useState([]);
-	const [videos, setVideos] = useState([]);
-	const [podcasts, setPodcasts] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const address = '/getLandingPage';
-	const fetcher = async (url) => await api.get(url).then(res => res.data);
-	const { data, error } = useSWR(address, fetcher);
-	useEffect(() => {
-		if (!data) return;
-		setLoading(false);
-		setCarouselItems(data.carousel);
-		setNews(data.news);
-		setVideos(data.videos);
-		setPodcasts(data.podcasts);
-	}, [data])
-	// useEffect(() => {
-
-	// 	(async function() {
-	// 		api.get('/getLandingPage').then(res => {
-	// 			setLoading(false);
-	// 			console.log(res.data)
-	// 			if (!res.data.success) return;
-	// 			setCarouselItems(res.data.carousel);
-	// 			setNews(res.data.news);
-	// 			setVideos(res.data.videos);
-	// 			setPodcasts(res.data.podcasts);
-	// 		})
-	// 	})()
-	// }, []);
-
+export default function Home({carouselItems, news, videos, podcasts}) {
 	return (
-		<Layout	Layout title='Home' carousel={carouselItems} noMargin loading={loading}>
+		<Layout	Layout title='Home' carousel={carouselItems} noMargin>
 			<Box sx={{mt: 4, mr: 4, ml: 4, mb:1}}>
 				<Grid item xs={12} sx={{display: 'flex', mb: 2}}>
 					<Typography component="span" variant="h5">
@@ -83,3 +51,18 @@ export default function Home() {
 		</Layout>
 	);
 };
+
+export async function getServerSideProps(context) {
+    return await api.get('/getLandingPage').then(res => {
+		if (res.data.success) {
+			return {
+				props: {
+					carouselItems: res.data.carousel,
+					videos: res.data.videos,
+					podcasts: res.data.podcasts,
+					news: res.data.news,
+				}
+			}
+		} 
+	});
+}
