246c8486e36bea9b0a9c04c7b56571b7237e52ba
Thiago Hagge <thiagohagge09@gmail.com>
2022-04-07 14:46:23 -0300

    new Build

--- pages/admin/podcasts/[slug].js
+++ pages/admin/podcasts/[slug].js
@@ -45,7 +45,7 @@ export async function getServerSideProps({query}) {
                     contact: contact
                 }
             }
-        }
+        } 
         return {
             props: {
                 pages: pages,
