08d214cda06eda57c065a9c06d3658c86b178d09
Thiago Hagge <thiago.hagge@ascbrazil.com.br>
2022-11-12 18:35:52 -0300

    remover manutenção

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    URL_API: 'https://academia-medicina-itabuna.herokuapp.com/'
+    URL_API: 'https://amei-ba.thiagohagge.com/'
+    // URL_API: 'http://localhost:8000/'
   }
 }
